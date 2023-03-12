0920fc89017c25ccac9e8c332e1eb10d691c69c2
Rej Averion <averionrej@gmail.com>
2023-03-12 11:53:03 +0400

    updated model hyperparameters

--- rps_train.py
+++ rps_train.py
@@ -1,3 +1,5 @@
+'''rps_train'''
+
 import json
 
 
@@ -14,11 +16,11 @@ def generate_play_data(player1, player2, player2_int, num_games):
     p2_prev_play = ""
 
     # for recording the moves of both programs
-    p1_last_20_moves = []
-    p2_last_20_moves = []
+    p1_last_20_moves = ""
+    p2_last_20_moves = ""
     training_data = []
     legal_moves = ["R", "P", "S"]
-    n = 10
+    n = 20
 
     for count in range(num_games):
         p1_play = player1(p2_prev_play)
@@ -29,8 +31,8 @@ def generate_play_data(player1, player2, player2_int, num_games):
 
         # Record the moves of both programs in batches of n moves.
         # This data will be used for training our bot to determine which program it is playing against
-        p1_last_20_moves.append(legal_moves.index(p1_play))
-        p2_last_20_moves.append(legal_moves.index(p2_play))
+        p1_last_20_moves += str(legal_moves.index(p1_play))
+        p2_last_20_moves += str(legal_moves.index(p2_play))
 
         # every n moves, append to the master list then start a new list
         if (count and ((count+1) % n == 0)):
@@ -40,11 +42,59 @@ def generate_play_data(player1, player2, player2_int, num_games):
                 "player2": player2_int
             }
             training_data.append(training_sample)
-            p1_last_20_moves = []
-            p2_last_20_moves = []
+            p1_last_20_moves = ""
+            p2_last_20_moves = ""
 
     # write training_data to a JSON file
     with open(f"training_data_{player2_int}.json", "w") as outfile:
         json.dump(training_data, outfile)
 
     return (training_data)
+
+
+# def train_model():
+#     '''train_model()'''
+#     df_0 = pd.read_json("training_data_0.json", dtype={
+#                         'player1_moves': str, 'player2_moves': str})  # player2 = quincy
+#     df_1 = pd.read_json("training_data_1.json", dtype={
+#                         'player1_moves': str, 'player2_moves': str})  # player2 = abbey
+#     df_2 = pd.read_json("training_data_2.json", dtype={
+#                         'player1_moves': str, 'player2_moves': str})  # player2 = kris
+#     df_3 = pd.read_json("training_data_3.json", dtype={
+#                         'player1_moves': str, 'player2_moves': str})  # player2 = mrugesh
+
+#     # concatenate the data from all 4 players
+#     df = pd.concat([df_0, df_1, df_2, df_3], axis=0).reset_index(drop=True)
+
+#     # split the dataframe into training and testing datasets
+#     X = df[['player1_moves', 'player2_moves']]
+#     y = df['player2']
+#     X_train, X_test, y_train, y_test = train_test_split(
+#         X, y, test_size=0.2, random_state=42)
+
+#     # Convert the input data to numpy arrays
+#     X = np.array([list(map(int, x)) for x in X])
+
+#     # Define the number of classes
+#     num_classes = len(np.unique(y))
+
+#     # Build the neural network model
+#     model = Sequential()
+#     model.add(Dense(64, input_dim=40, activation='relu'))
+#     model.add(Dense(32, activation='relu'))
+#     model.add(Dense(num_classes, activation='softmax'))
+
+#     # Compile the model
+#     model.compile(loss='sparse_categorical_crossentropy',
+#                   optimizer='adam', metrics=['accuracy'])
+
+#     # Train the model
+#     model.fit(X_train, y_train, epochs=50, batch_size=32,
+#               validation_data=(X_test, y_test))
+
+#     # Evaluate the model on the testing set
+#     score = model.evaluate(X_test, y_test, verbose=0)
+#     print('Test loss:', score[0])
+#     print('Test accuracy:', score[1])
+
+#     return None
